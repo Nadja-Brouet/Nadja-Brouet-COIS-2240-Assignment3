1860dbbb679ef7b32423992e1bd000529b4fce89
Nadja-Brouet <Nadjabrouet2@gmail.com>
2024-12-04 01:34:42 -0500

    Task 2 Subtask 4
    Change addMember() and addId() to boolean instead of void. add true and false return. create if statements using findMemberById and findBookById in their respective methods to check if the Id's already exist, if Id's exist return false. if Id doesn't already exist add member/book and return true.

--- Library.java
+++ Library.java
@@ -2,46 +2,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
-    private List<Member> members = new ArrayList<Member>();
-    private List<Book> books = new ArrayList<Book>();
+	private List<Member> members = new ArrayList<Member>();
+	private List<Book> books = new ArrayList<Book>();
 
-    // Add a new member to the library
-    public void addMember(Member member) {
-        members.add(member);
-    }
-    
-    // Add a new book to the library
-    public void addBook(Book book) {
-        books.add(book);
-    }
+	// Add a new member to the library
+	public boolean addMember(Member member) {
 
-    // Find a member by ID
-    public Member findMemberById(int id) {
-        for (Member member : members) {
-            if (member.getId() == id) {
-                return member;
-            }
-        }
-        return null;
-    }
+		// check if member ID already exits
+		if (findMemberById(member.getId()) != null) {
+			System.out.println("Member" + member.getId() + "already exists");
+			return false;
+		}
 
-    // Find a book by ID
-    public Book findBookById(int id) {
-        for (Book book : books) {
-            if (book.getId() == id) {
-                return book;
-            }
-        }
-        return null;
-    }
+		members.add(member);
+		return true;
+	}
 
-    // Get the list of members
-    public List<Member> getMembers() {
-        return members;
-    }
-    
-    // Get the list of books
-    public List<Book> getBooks() {
-        return books;
-    }
+	// Add a new book to the library
+	public boolean addBook(Book book) {
+
+		// check if book ID already exists
+		if (findBookById(book.getId()) != null) {
+			System.out.println("Book with ID" + book.getId() + "already exists");
+			return false;
+		}
+		books.add(book);
+		return true;
+	}
+
+	// Find a member by ID
+	public Member findMemberById(int id) {
+		for (Member member : members) {
+			if (member.getId() == id) {
+				return member;
+			}
+		}
+		return null;
+	}
+
+	// Find a book by ID
+	public Book findBookById(int id) {
+		for (Book book : books) {
+			if (book.getId() == id) {
+				return book;
+			}
+		}
+		return null;
+	}
+
+	// Get the list of members
+	public List<Member> getMembers() {
+		return members;
+	}
+
+	// Get the list of books
+	public List<Book> getBooks() {
+		return books;
+	}
 }
